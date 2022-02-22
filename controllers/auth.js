2ad6e1a72a82006328a71504ad4e5753b6801aa1
Emin <eminshamshoian@gmail.com>
2022-02-21 20:44:27 -0800

    created reset password and connected to AWS SES email services

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,88 +1,84 @@
-import jwt from 'jsonwebtoken';
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+import AWS from "aws-sdk";
 
-// Impport User model
-import User from '../models/user';
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
 
-// Import passowrd hashing utilities
-import { hashPassword, comparePassword } from '../utils/auth';
+const SES = new AWS.SES(awsConfig);
 
 export const register = async (req, res) => {
   try {
-    // Destructure the request body from frontend
+    // console.log(req.body);
     const { name, email, password } = req.body;
-    // Validate the user name and password
-    if (!name) return res.status(400).send('Name is required');
+    // validation
+    if (!name) return res.status(400).send("Name is required");
     if (!password || password.length < 6) {
       return res
         .status(400)
-        .send('Password is required and should be min 6 characters long');
+        .send("Password is required and should be min 6 characters long");
     }
-
-    // Check if user exists
     let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send("Email is taken");
 
-    // Check if email is already in use
-    if (userExist) return res.status(400).send('Credentials already taken!');
-
-    // Hash the password and save it in a variable
+    // hash password
     const hashedPassword = await hashPassword(password);
 
-    // Register the user with proper hashed password
+    // register
     const user = new User({
       name,
       email,
       password: hashedPassword,
     });
-
-    // Save the user in the database
     await user.save();
-
-    // Return a ok response
+    // console.log("saved user", user);
     return res.json({ ok: true });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send('Error with registration');
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
   }
 };
 
 export const login = async (req, res) => {
   try {
-    // Destructure
+    // console.log(req.body);
     const { email, password } = req.body;
-
-    // Find user by searching against db
+    // check if our db has user with that email
     const user = await User.findOne({ email }).exec();
-    if (!user) return res.status(400).send('Credentials Not Found');
-
-    // Check password
+    if (!user) return res.status(400).send("No user found");
+    // check password
     const match = await comparePassword(password, user.password);
+    if (!match) return res.status(400).send("Wrong password");
 
-    // Create signed JWT
+    // create signed jwt
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
+      expiresIn: "7d",
     });
-
-    // Return user and token to client, exclude hashed password
+    // return user and token to client, exclude hashed password
     user.password = undefined;
-
-    // Send token in cookie with httponly flag so its not accesible with JS
-    res.cookie('token', token, {
+    // send token in cookie
+    res.cookie("token", token, {
       httpOnly: true,
       // secure: true, // only works on https
     });
-
-    // Send user as json response
+    // send user as json response
     res.json(user);
   } catch (err) {
     console.log(err);
-    return res.status(400).send('There was an Error Logging In, Try Again!');
+    return res.status(400).send("Error. Try again.");
   }
 };
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie('token');
-    return res.json({ message: 'Logout Succesful!' });
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
   } catch (err) {
     console.log(err);
   }
@@ -90,9 +86,85 @@ export const logout = async (req, res) => {
 
 export const currentUser = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select('-password').exec();
+    const user = await User.findById(req.user._id).select("-password").exec();
+    console.log("CURRENT_USER", user);
     return res.json({ ok: true });
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    // console.log(email);
+    const shortCode = nanoid(6).toUpperCase();
+    const user = await User.findOneAndUpdate(
+      { email },
+      { passwordResetCode: shortCode }
+    );
+    if (!user) return res.status(400).send("User not found");
+
+    // prepare for email
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `
+                <html>
+                  <h1>Reset password</h1>
+                  <p>User this code to reset your password</p>
+                  <h2 style="color:red;">${shortCode}</h2>
+                  <i>edemy.com</i>
+                </html>
+              `,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Reset Password",
+        },
+      },
+    };
+
+    const emailSent = SES.sendEmail(params).promise();
+    emailSent
+      .then((data) => {
+        console.log(data);
+        res.json({ ok: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { email, code, newPassword } = req.body;
+    // console.table({ email, code, newPassword });
+    const hashedPassword = await hashPassword(newPassword);
+
+    const user = User.findOneAndUpdate(
+      {
+        email,
+        passwordResetCode: code,
+      },
+      {
+        password: hashedPassword,
+        passwordResetCode: "",
+      }
+    ).exec();
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error! Try again.");
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,25 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-// Middlewares
-import { requireSignin } from '../middlewares';
+// middleware
+import { requireSignin } from "../middlewares";
 
 // controllers
-import { register, login, logout, currentUser } from '../controllers/auth';
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/auth";
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/logout', logout);
-router.get('/current-user', requireSignin, currentUser);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/current-user", requireSignin, currentUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
